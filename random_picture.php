d199ecc03107eb8e8af446a877e548a1b2707553
Jan Paul Posma <jp.posma@gmail.com>
2010-01-08 22:04:22 +0100

    Nicer pictures

--- random_picture.php
+++ random_picture.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("config.php");
+require_once('config.php');
 
 function findDirsRecursive($path, array &$dirs) 
 {
@@ -23,7 +23,13 @@ function findDirsRecursive($path, array &$dirs)
 function newDir($paths)
 {
 	$dirs = array();
-	foreach ($paths as $path) findDirsRecursive($path, $dirs);
+	foreach ($paths as $path) 
+	{
+		// strip trailing slashes
+		if (substr($path, -1) == '/' || substr($path, -1) == '\\') $path = substr($path, 0, -1);
+		 
+		findDirsRecursive($path, $dirs);
+	}
 	
 	return $dirs[rand(0, count(dirs)-1)];
 }
@@ -36,7 +42,7 @@ function findFiles($path)
 		$files = array();
         while (false !== ($file = readdir($dir)))
 		{
-			if (strtolower(substr($file,strlen($file)-4,4)) == ".jpg") 
+			if (strtolower(substr($file,strlen($file)-4,4)) == '.jpg') 
 			{
 				$files[] = $path . '/' . $file;
 			}
@@ -53,13 +59,13 @@ $dir       = @$_SESSION['display.dir'];
 $dircount  = @$_SESSION['display.dircount'];
 
 if (strlen($dir) <= 0 || file_exists($dir)) {
-	$dir      = newDir($ImageDirs);
+	$dir      = newDir($settings['photo.dirs']);
 	$dircount = 0;
 }
 
 $files = findFiles($dir);
 while ($dircount > 10 || $dircount >= count($files) || count($files) <= 0) {
-	$dir      = newDir($ImageDirs);
+	$dir      = newDir($settings['photo.dirs']);
 	$dircount = 0;
 	$files    = findFiles($dir);
 }
@@ -74,11 +80,26 @@ $width  = $_REQUEST['width'];
 $height = $_REQUEST['height'];
 
 // Info string
-$info  = $file;
-$info .= $Settings["photo.copyright"];
+$info  = '';
+
+if ($settings['photo.show_filename'])
+{
+	$infoFilename = $file;
+	if ($settings['photo.format_filename'])
+	{
+		$infoFilename = str_ireplace($settings['photo.dirs'], '',    $infoFilename);
+		$infoFilename = str_ireplace('.jpg',                  '',    $infoFilename);
+		$infoFilename = str_ireplace(array('/', '\\'),        ' - ', $infoFilename);
+		$infoFilename = str_ireplace(array('_', '.'),         ' ',   $infoFilename);
+	}
+	
+	$info .= $infoFilename;
+}
+
+$info .= $settings['photo.copyright'];
 
 // make GD work
-header("Content-type: image/jpeg");
+header('Content-type: image/jpeg');
 $picture = imagecreatefromjpeg($file);
 
 $img = imagecreatetruecolor($width, $height);
@@ -96,7 +117,7 @@ $dst_height = round($src_height * $ratio);
 
 imagecopyresampled($img, $picture, ($width - $dst_width)/2, ($height - $dst_height)/2, 0,0, $dst_width, $dst_height, $src_width, $src_height);
 
-imagestring($img,$Settings["photo.fontsize"],($width / 2) - ((strlen($info) * imagefontwidth($Settings["photo.fontsize"])) / 2), $height-20,$info,$shadow);
-imagestring($img,$Settings["photo.fontsize"],($width / 2) - ((strlen($info) * imagefontwidth($Settings["photo.fontsize"])) / 2) - 1, $height-21,$info,$col);
+imagestring($img,$settings['photo.fontsize'],($width / 2) - ((strlen($info) * imagefontwidth($settings['photo.fontsize'])) / 2), $height-20,$info,$shadow);
+imagestring($img,$settings['photo.fontsize'],($width / 2) - ((strlen($info) * imagefontwidth($settings['photo.fontsize'])) / 2) - 1, $height-21,$info,$col);
 
 imagejpeg($img, NULL, 100);
