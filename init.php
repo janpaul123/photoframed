cba0bf2ea1e213efe300c785ab2113e9926a4c99
Jan Paul Posma <jp.posma@gmail.com>
2010-01-09 12:19:10 +0100

    Nicer readme, nicer cache failure instructions

--- init.php
+++ init.php
@@ -5,4 +5,9 @@ if (!file_exists(dirname(__FILE__) . '/user/config.php'))
 	die('Please check your photogenix/user folder and rename one of the configuration templates to config.php');
 }
 
+if (!is_writeable(dirname(__FILE__) . '/cache'))
+{
+	die('Make sure that the cache directory is writeable!<br> Run this in the console: <strong>chmod -R 0777 cache</strong> or <strong>sudo chmod -R 0777 cache</strong>');
+}
+
 require_once(dirname(__FILE__) . '/user/config.php');
